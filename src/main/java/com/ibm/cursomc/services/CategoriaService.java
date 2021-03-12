83cc9efe00bb0410705c7fa4ebe14cbf02678cca
Luanda Brito <49211714+luandabrito@users.noreply.github.com>
2021-03-12 20:04:29 -0300

    Criando repository e service para Categoria

---
+++ src/main/java/com/ibm/cursomc/services/CategoriaService.java
@@ -0,0 +1,23 @@
+package com.ibm.cursomc.services;
+
+import com.ibm.cursomc.domain.Categoria;
+import com.ibm.cursomc.repositories.CategoriaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class CategoriaService {
+
+    @Autowired
+    private CategoriaRepository repo;
+
+    public Categoria buscar(Integer id) {
+
+        Optional<Categoria> obj = repo.findById(id);
+
+        return obj.orElse(null);
+    }
+
+}
