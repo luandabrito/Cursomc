7a11b51888ff754f7408214307129572295c6865
Luanda Brito <49211714+luandabrito@users.noreply.github.com>
2021-03-12 21:00:02 -0300

    Produto e associacao muitos para muitos

--- src/main/java/com/ibm/cursomc/CursomcApplication.java
+++ src/main/java/com/ibm/cursomc/CursomcApplication.java
@@ -1,7 +1,9 @@
 package com.ibm.cursomc;
 
 import com.ibm.cursomc.domain.Categoria;
+import com.ibm.cursomc.domain.Produto;
 import com.ibm.cursomc.repositories.CategoriaRepository;
+import com.ibm.cursomc.repositories.ProdutoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -15,6 +17,9 @@ public class CursomcApplication implements CommandLineRunner {
     @Autowired
     private CategoriaRepository categoriaRepository;
 
+    @Autowired
+    private ProdutoRepository produtoRepository;
+
     public static void main(String[] args) {
         SpringApplication.run(CursomcApplication.class, args);
     }
@@ -22,11 +27,23 @@ public class CursomcApplication implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
 
-        Categoria cat1 = new Categoria("Informatica");
-        Categoria cat2 = new Categoria("Escritorio");
+        Categoria cat1 = new Categoria(null, "Informatica");
+        Categoria cat2 = new Categoria(null, "Escritorio");
+
+        Produto p1 = new Produto(null, "Computador", 2000.00);
+        Produto p2 = new Produto(null, "Impressora", 800.00);
+        Produto p3 = new Produto(null, "Mouse", 80.00);
+
+        cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
+        cat2.getProdutos().addAll(Arrays.asList(p2));
+
+        p1.getCategorias().addAll(Arrays.asList(cat1));
+        p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
+        p3.getCategorias().addAll(Arrays.asList(cat1));
 
         categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
-
+        produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
 
     }
 }
+0
