a5b6a0247943ec3b543dc1a6bc3fb8196c092ca0
Luanda Brito <49211714+luandabrito@users.noreply.github.com>
2021-03-15 10:59:08 -0300

    Estado e cidade

---
+++ src/main/java/com/ibm/cursomc/repositories/EstadoRepository.java
@@ -0,0 +1,10 @@
+package com.ibm.cursomc.repositories;
+
+import com.ibm.cursomc.domain.Estado;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EstadoRepository extends JpaRepository<Estado, Integer> {
+
+}
